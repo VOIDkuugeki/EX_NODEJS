86df21557e068b43c6b2ac21be4f50a1dc6bf222
VOIDkuugeki <voidkuugeki@gmail.com>
2024-10-15 14:03:53 +0700

    first commit

---
+++ tinhChuviDientich.js
@@ -0,0 +1,8 @@
+var l = 10;
+var w = 5;
+
+const perimeterCal = (l, w) => 2 * (l + w);
+const areaCal = (l, w) => l * w;
+
+console.log(`Chu vi hinh chu nhat la: ${perimeterCal(l, w)}`);
+console.log(`Dien tich hinh chu nhat la: ${areaCal(l, w)}`);
