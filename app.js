fe990e008f3c1e86a4a94fc01116d2b8d82b7efb
VOIDkuugeki <voidkuugeki@gmail.com>
2024-11-14 17:38:15 +0700

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-const express = require('express');							
-const bodyParser = require('body-parser');							
-const rectangleRoutes = require('./routes/rectangleRoutes');							
-							
-const app = express();							
-const PORT = process.env.PORT || 5000;							
-							
-// Sử dụng EJS làm view engine							
-app.set('view engine', 'ejs');							
-app.set('views', './views');							
-							
-// Middleware							
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.render('index', { perimeter: null, area: null });
-})
-							
-// Sử dụng routes							
-app.use('/rectangle', rectangleRoutes);							
-							
-// Chạy ứng dụng							
-app.listen(PORT, () => {							
-console.log(`Server is running on http://localhost:${PORT}`);							
-});							
+require('dotenv').config();										
+const express = require('express');										
+const bodyParser = require('body-parser');										
+const squareRoutes = require('./routes/squareRoutes');										
+										
+const app = express();										
+const PORT = process.env.PORT || 3000;										
+										
+// Sử dụng EJS làm view engine										
+app.set('view engine', 'ejs');										
+app.set('views', './views');										
+										
+// Middleware										
+app.use(bodyParser.urlencoded({ extended: true }));										
+										
+// Sử dụng routes										
+app.use('/', squareRoutes);										
+										
+// Chạy ứng dụng										
+app.listen(PORT, () => {										
+console.log(`Server is running on http://localhost:${PORT}`);										
+});										
