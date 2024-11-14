0b10396e7b996f81dcfd9ad6dd8c622164dd3c6b
VOIDkuugeki <voidkuugeki@gmail.com>
2024-11-14 18:15:25 +0700

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-require('dotenv').config();										
-const express = require('express');										
-const bodyParser = require('body-parser');										
-const squareRoutes = require('./routes/squareRoutes');										
-										
-const app = express();										
-const PORT = process.env.PORT || 3000;										
-										
-// Sử dụng EJS làm view engine										
-app.set('view engine', 'ejs');										
-app.set('views', './views');										
-										
-// Middleware										
-app.use(bodyParser.urlencoded({ extended: true }));										
-										
-// Sử dụng routes										
-app.use('/', squareRoutes);										
-										
-// Chạy ứng dụng										
-app.listen(PORT, () => {										
-console.log(`Server is running on http://localhost:${PORT}`);										
-});										
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+const PORT = 3000;
+
+// Kết nối tới MongoDB
+mongoose
+  .connect("mongodb+srv://vinhpham220212:vinh13012004@cluster0.w6qru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+// Cấu hình middleware
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+
+// Sử dụng routes
+app.use("/products", productRoutes);
+
+// Khởi động server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
